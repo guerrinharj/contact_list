57af858ddff8349cf5cb1c2ae903ea6c5eb41d6b
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-01-19 07:25:51 -0300

    added factorybot and created basic testing for models

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+    factory :contact do
+        association :user
+        name { Faker::Name.name }
+        tax_number { CPF.generate }
+        phone { Faker::PhoneNumber.phone_number }
+        address_name { Faker::Address.street_name }
+        address_number { Faker::Address.building_number }
+        address_complement { Faker::Address.secondary_address }
+        postal_code { '01310-100' }
+        latitude { Faker::Address.latitude }
+        longitude { Faker::Address.longitude }
+    end
+end
